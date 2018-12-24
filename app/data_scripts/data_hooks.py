0d9dde9cba9ed368a21acff12613f880e0f0a33b
gagejustins <gagejustins@gmail.com>
2018-12-24 16:44:41 +0000

    add list manufacturers and update list sneakers to include manufacturer

--- app/data_scripts/data_hooks.py
+++ app/data_scripts/data_hooks.py
@@ -17,13 +17,23 @@ def db_connect(DBURI=os.environ['DATABASE_URL']):
         return None
 
 def list_available_sneakers(conn):
-    sql = "SELECT id, CONCAT_WS(', ', sneaker_name, color) FROM sneakers WHERE sold_at IS NULL"
+    sql = "select s.id, concat_ws(', ',concat_ws(' ', case when m.collaborator_name is not null then concat_ws(' x ', m.manufacturer_name, m.collaborator_name) else m.manufacturer_name end, s.sneaker_name), s.color) as sneaker from sneakers s join manufacturers m on s.manufacturer_id = m.id and s.sold_at is null and s.trashed_at is null and s.given_at is null"
+
     cur = conn.cursor()
     cur.execute(sql)
     results = cur.fetchall()
 
     sneakers = [result for result in results]
     return sneakers
+
+def list_available_manufacturers(conn):
+    sql="SELECT id, CASE WHEN collaborator_name IS NOT NULL THEN CONCAT_WS(' x ', manufacturer_name, collaborator_name) ELSE manufacturer_name END from manufacturers ORDER BY 2"
+    cur = conn.cursor()
+    cur.execute(sql)
+    results = cur.fetchall()
+ 
+    manufacturers = [result for result in results]
+    return manufacturers
 
 def sneaker_event_insert(conn, sneaker_id, event_type, config):
 
@@ -52,3 +62,40 @@ def sneaker_insert(conn, sneaker_name, color, purchase_price, manufacturer_id):
     conn.commit()
 
     return result_id 
+
+def manufacturer_insert(conn, manufacturer_name, collaborator_name):
+
+    #If collaborator_name is empty, set it to NULL
+    if collaborator_name == '':
+        collaborator_name = None
+    
+    sql = "INSERT INTO manufacturers VALUES (DEFAULT, %s, %s, %s) RETURNING id"
+    params = (manufacturer_name, collaborator_name, datetime.utcnow())
+
+    cur = conn.cursor()
+    cur.execute(sql, params)
+    result_id = cur.fetchone()[0]
+    conn.commit()
+
+    return result_id
+
+def sneaker_remove(conn, sneaker_id, remove_type):
+    
+    remove_col_mappings = {'sell': "sold_at", 'trash': "trashed_at", 'give': "given_at"}
+    remove_col = remove_col_mappings[remove_type]
+    
+    if remove_col == "sold_at":
+        sql = "UPDATE sneakers SET sold_at = %s WHERE id = %s"
+    elif remove_col == "trashed_at":
+        sql = "UPDATE sneakers SET trashed_at = %s WHERE id = %s"
+    else:
+        sql = "UPDATE sneakers SET given_at = %s WHERE id = %s"
+
+    params = (datetime.utcnow(), sneaker_id)
+
+    cur = conn.cursor()
+    cur.execute(sql, params)
+    conn.commit()
+
+    return "Executed " + sql 
+
