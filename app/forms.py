5f12e3c257f021c21b2c38f32d3b5491f7833649
gagejustins <gagejustins@gmail.com>
2018-12-24 16:45:47 +0000

    add constructor and initialize choices there

--- app/forms.py
+++ app/forms.py
@@ -4,19 +4,37 @@ from wtforms.validators import DataRequired
 from app.data_scripts import data_hooks
 
 class SneakerEventForm(FlaskForm):
-    event_type = SelectField('Event Name', validators=[DataRequired()], choices=[('add', 'Add'), ('remove', 'Remove'), ('clean', 'Clean'), ('wear', 'Wear')])
-    owner_id = SelectField('OwnerId', validators=[DataRequired()], choices= [(0,0)])
-    sneaker_id = SelectField('SneakerId', validators=[DataRequired()], choices = [(1,1)])
+    event_type = SelectField('Event Name', validators=[DataRequired()], choices=[('clean', 'Clean'), ('wear', 'Wear'), ('walk', 'Walk')])
+    #owner_id = SelectField('OwnerId', coerce=int, validators=[DataRequired()], choices= [(0,0)])
+    sneaker_id = SelectField('Sneaker Name', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Log Event')    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sneaker_id.choices = data_hooks.list_available_sneakers(data_hooks.db_connect())
 
 class SneakerAddForm(FlaskForm):
     sneaker_name = StringField(validators=[DataRequired()]) 
     color = StringField(validators=[DataRequired()])
     purchase_price = FloatField(validators=[DataRequired()]) 
-    manufacturer_id = IntegerField(validators=[DataRequired()]) 
+    manufacturer_id = SelectField('Manufacturer Name', coerce=int, validators=[DataRequired()]) 
     submit = SubmitField('Add Sneaker') 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.manufacturer_id.choices = data_hooks.list_available_manufacturers(data_hooks.db_connect())
+
+class ManufacturerAddForm(FlaskForm):
+    manufacturer_name = StringField(validators=[DataRequired()])
+    collaborator_name = StringField(default=None)
+    submit = SubmitField('Add Manufacturer')
 
 class SneakerRemoveForm(FlaskForm):
     removal_type = SelectField('Removal Type', validators=[DataRequired()], choices=[('sell', 'Sell'), ('trash', 'Trash'), ('give', 'Give')])
-    sneaker_to_remove = SelectField('Sneaker Name', coerce=int, validators=[DataRequired()], choices=[result for result in data_hooks.list_available_sneakers(data_hooks.db_connect())])
+    sneaker_to_remove = SelectField('Sneaker Name', coerce=int, validators=[DataRequired()]) 
     submit = SubmitField('Remove Sneaker')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sneaker_to_remove.choices = data_hooks.list_available_sneakers(data_hooks.db_connect())
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import SneakerEventForm, SneakerAddForm, SneakerRemoveForm
+from app.forms import SneakerEventForm, SneakerAddForm, SneakerRemoveForm, ManufacturerAddForm
 from app.data_scripts import data_hooks 
 
 @app.route('/')
@@ -13,6 +13,10 @@ def index():
 def log_sneaker_event():
     form = SneakerEventForm()
     if form.validate_on_submit():
+        conn = data_hooks.db_connect()
+        event = data_hooks.sneaker_event_insert(conn, form.sneaker_id.data, form.event_type.data, config=data_hooks.readConfigs())
+        flash(event)
+        conn.close()
         return redirect(url_for('index'))
     return render_template('sneaker_event.html', title='Event logged', form=form)
 
@@ -27,7 +31,18 @@ def add_sneakers():
         flash('Inserted ID: ' + str(inserted_id))
         conn.close()
         return redirect(url_for('index'))
-    return render_template('sneaker_catalog.html', title='Event logged', form=form)
+    return render_template('sneaker_add.html', title='Event logged', form=form)
+
+@app.route('/add-manufacturer', methods=['GET', 'POST'])
+def add_manufacturer():
+    form = ManufacturerAddForm()
+    if form.validate_on_submit():
+        conn = data_hooks.db_connect()
+        inserted_id = data_hooks.manufacturer_insert(conn, form.manufacturer_name.data, form.collaborator_name.data)
+        flash(inserted_id)
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('manufacturer_add.html', title='Event logged', form=form) 
 
 @app.route('/remove-sneakers', methods=['GET', 'POST'])
 def remove_sneakers():
@@ -35,7 +50,8 @@ def remove_sneakers():
     if form.validate_on_submit():
         conn = data_hooks.db_connect()
         event = data_hooks.sneaker_event_insert(conn, form.sneaker_to_remove.data, form.removal_type.data, config=None)
-        flash(event)
+        remove_update = data_hooks.sneaker_remove(conn, form.sneaker_to_remove.data, form.removal_type.data)
+        flash(event, remove_update)
         conn.close()
         return redirect(url_for('index'))
     return render_template('sneaker_remove.html', title='Event logged', form=form)
